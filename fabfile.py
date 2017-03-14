5c4dc57c8e0c91f4f3cf34d0c30f8c4e14c2a139
haibo <393993705@qq.com>
2017-03-14 10:46:46 +0800

    testssh

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8 -*-
+
+import os
+from fabric.api import local,lcd
+
+def pushcode(msg):
+    with lcd(os.path.join(os.path.abspath('.'))):
+        local("git commit -m %s"% msg)
+        local(("git push origin master"))
+
+
+
